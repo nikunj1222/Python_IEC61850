766c9b69677dbb92ae846813210dbc438d498853
Nikunj Patel <nikunjpatel1222@gmail.com>
2022-10-06 18:08:13 +0200

    Update 61850_Control_command.py

--- 61850_Control_command.py
+++ 61850_Control_command.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-from ast import If
+#from ast import If
 import os,sys,time
 from winreg import SetValue
 import iec61850
 import iecClientCtrl
+from datetime import datetime
 
 if __name__=="__main__":
         hostname = "192.20.30.1";
@@ -26,29 +27,41 @@ if __name__=="__main__":
         '''
         if error == iec61850.IED_ERROR_OK :
                 controlCmd = iecClientCtrl.SBOctrlDPCEnhanced(ctrlpath=ctrlpath,iedconnerr=error,iedconnection=con,
-                                                 selectValue=False,operateValue=True,cancelctrl=False,ILKBit=True,SYNCBit=False,TestBit=False,operctrltimeafterselect=1,
-                                                 cmdtimeout=1,cmdCategory=3,cmdIdentifier='script123yoyo',selectctlNum=24,operatectlNum=24)
-                
+                                                 selectValue=False,operateValue=False,cancelctrl=False,ILKBit=True,SYNCBit=False,TestBit=False,operctrltimeafterselect=1,
+                                                 cmdtimeout=1,cmdCategory=3,cmdIdentifier='script',selectctlNum=24,operatectlNum=24)
+                '''controlCmd = iecClientCtrl.SBOctrlDPCNormal(ctrlpath=ctrlpath,iedconnerr=error,iedconnection=con
+                                                 ,operateValue=True,cancelctrl=False,operctrltimeafterselect=1,
+                                                 cmdtimeout=1,cmdCategory=3,cmdIdentifier='script123yoyo',selectctlNum=24,operatectlNum=24)'''
+                print('AddCause : ',controlCmd[0],'Error : ',controlCmd[1])
+
+                DPS = iecClientCtrl.ReadDPSValue(con,error,ctrlpath)
+                print(DPS[0],DPS[1],DPS[2])
+
+                SPS = iecClientCtrl.ReadSPSValue(con,error,'TESTP30Control/CILO1.EnaOpn')
+                print(SPS[0],SPS[1],SPS[2])
+
+                INTST = iecClientCtrl.ReadINTValue(con,error,'TESTP30Control/CSWI1.Mod')
+                print(INTST[0],INTST[1],INTST[2])
+
+                INTST = iecClientCtrl.ReadINTValue(con,error,'TESTP30Control/XCBR1.CBOpCap')
+                print(INTST[0],INTST[1],INTST[2])
+
+                FLOATST = iecClientCtrl.ReadFLOATValue(con,error,'TESTP30Measurements/MmuPriMMXU1.TotW')
+                print(FLOATST[0],FLOATST[1],FLOATST[2])
+
+                STRST = iecClientCtrl.ReadSTRINGValue(con,error,'TESTP30Control/CSWI1.Pos.SBOw.origin.orIdent')
+                print(STRST)
+
+                value = iec61850.MmsValue_newInteger(2)
+                iec61850.IedConnection_writeObject(con,'TESTP30System/LLN0.Mod.ctlModel',iec61850.IEC61850_FC_CF,value)
+
+       
         else :
                 print('IED connection failed')
 
-        print('AddCause : ',controlCmd[0],'Error : ',controlCmd[1])
+        ArithmeticError
 
-        DPS = iecClientCtrl.ReadDPSValue(con,error,ctrlpath)
-        print(DPS[0],DPS[1])
-
-        SPS = iecClientCtrl.ReadSPSValue(con,error,'TESTP30Control/CILO1.EnaOpn')
-        print(SPS[0],SPS[1])
-
-        INTST = iecClientCtrl.ReadINTValue(con,error,'TESTP30Control/CSWI1.Mod')
-        print(INTST[0],INTST[1])
-
-        FLOATST = iecClientCtrl.ReadFLOATValue(con,error,'TESTP30Measurements/MmuPriMMXU1.TotW')
-        print(FLOATST[0],FLOATST[1])
-
-        STRST = iecClientCtrl.ReadSTRINGValue(con,error,'TESTP30Control/CSWI1.Pos.SBOw.origin.orIdent')
-        print(STRST)
-
+        
 
                 
         iec61850.IedConnection_destroy(con)
