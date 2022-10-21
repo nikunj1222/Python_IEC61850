a13967feec0fedf9b1b0317ba3aab9db33807755
Nikunj Patel <nikunjpatel1222@gmail.com>
2022-10-21 12:41:29 +0200

    Possible to publish GOOSE with DPS and Quality

--- iecGOOSEctrl.py
+++ iecGOOSEctrl.py
@@ -1,21 +1,58 @@
 import os,sys,time
 from winreg import SetValue
-
+from xmlrpc.client import boolean
 from pyautogui import FAILSAFE
 import iec61850
-from datetime import datetime
+#from datetime import datetime
+
+def Quality_intvalue(quality):
+        if quality == 'QUALITY_VALIDITY_GOOD':
+                Quality_int = 0
+        elif quality == 'QUALITY_VALIDITY_INVALID':
+                Quality_int = 2
+        elif quality == 'QUALITY_VALIDITY_RESERVED':
+                Quality_int = 1
+        elif quality == 'QUALITY_VALIDITY_QUESTIONABLE':
+                Quality_int = 3
+        elif quality == 'QUALITY_DETAIL_OVERFLOW':
+                Quality_int = 4
+        elif quality == 'QUALITY_DETAIL_OUT_OF_RANGE':
+                Quality_int = 8
+        elif quality == 'QUALITY_DETAIL_BAD_REFERENCE':
+                Quality_int = 16
+        elif quality == 'QUALITY_DETAIL_OSCILLATORY':
+                Quality_int = 32
+        elif quality == 'QUALITY_DETAIL_FAILURE':
+                Quality_int = 64
+        elif quality == 'QUALITY_DETAIL_OLD_DATA':
+                Quality_int = 128
+        elif quality == 'QUALITY_DETAIL_INCONSISTENT':
+                Quality_int = 256
+        elif quality == 'QUALITY_DETAIL_INACCURATE':
+                Quality_int = 512
+        elif quality == 'QUALITY_SOURCE_SUBSTITUTED':
+                Quality_int = 1024
+        elif quality == 'QUALITY_TEST':
+                Quality_int = 2048
+        elif quality == 'QUALITY_OPERATOR_BLOCKED':
+                Quality_int = 4096
+        elif quality == 'QUALITY_DERIVED':
+                Quality_int = 8192
+        else :
+                Quality_int = 0  
+        return Quality_int
 
 class goosectrl:
         
         GOOSEdatasetlistupdate =  [True, False, True, False, True]
-        GOOSEdatasetlistdefault = [True, False, True, False, True]
+        GOOSEdatasetlistdefault = [True, False, False, False, False, False, False, False, False, False, 'INTERMEDIATE', True, 'QUALITY_TEST'] 
         stNum = 1
 
         def __init__(self,interfaceid='2',GOOSEappid=1000, GOOSEvlanId=1, GOOSEvlanPriority=7, GOOSEctrlblkname='simpleIOGenericIO/LLN0$GO$gcbAnalogValues',
                         GOOSEconfRev=1, GOOSEdatasetRef='simpleIOGenericIO/LLN0$AnalogValues', TimeAllowedToLive=500, mintime=10, maxtime=1000, timeincreament=10, vlantagstatus=False):                
                 self.interfaceid = interfaceid                                 
                 self.gooseCommParameters =  iec61850.CommParameters()
-                iec61850.CommParameters_setDstAddress(self.gooseCommParameters,0x01,0x0c,0xcd,0x01,0x01,0x01)
+                iec61850.CommParameters_setDstAddress(self.gooseCommParameters,0x01,0x0c,0xcd,0x01,0x00,0x01)
                 self.gooseCommParameters.appId  = GOOSEappid
                 self.gooseCommParameters.vlanId = GOOSEvlanId
                 self.gooseCommParameters.vlanPriority = GOOSEvlanPriority
@@ -32,7 +69,26 @@ class goosectrl:
         '''Create Goose dataset. Function input list of FCDA value and iec61850 dataset instance '''
         def createdatasetFCDA (self,fcdalist, datasetvalueVar):
                 for l in range (len(fcdalist)):
-                        iec61850.LinkedList_add(datasetvalueVar, iec61850.MmsValue_newBoolean(fcdalist[l]))
+                        if fcdalist[l] == False:
+                                iec61850.LinkedList_add(datasetvalueVar, iec61850.MmsValue_newBoolean(fcdalist[l]))
+                        elif fcdalist[l] == True:
+                                iec61850.LinkedList_add(datasetvalueVar, iec61850.MmsValue_newBoolean(fcdalist[l]))
+                        elif fcdalist[l] == 'INTERMEDIATE':
+                                valueof_l = 0
+                                iec61850.LinkedList_add(datasetvalueVar, iec61850.Dbpos_toMmsValue(None, valueof_l))
+                        elif fcdalist[l] == 'OPEN':
+                                valueof_l = 1
+                                iec61850.LinkedList_add(datasetvalueVar, iec61850.Dbpos_toMmsValue(None, valueof_l))
+                        elif fcdalist[l] == 'CLOSE':
+                                valueof_l = 2
+                                iec61850.LinkedList_add(datasetvalueVar, iec61850.Dbpos_toMmsValue(None, valueof_l))
+                        elif fcdalist[l] == 'UNDEFINED':
+                                valueof_l = 3
+                                iec61850.LinkedList_add(datasetvalueVar, iec61850.Dbpos_toMmsValue(None, valueof_l))
+                        elif 'QUALITY' in fcdalist[l] :
+                                mmsval = iec61850.MmsValue_newBitString(13)
+                                iec61850.MmsValue_setBitStringFromIntegerBigEndian(mmsval,Quality_intvalue(fcdalist[l]))
+                                iec61850.LinkedList_add(datasetvalueVar, mmsval)
 
         '''Remove existing FCDA in dataset and update it with new FCDA'''                
         def publishupdatedGOOSE (self,fcdalist, datasetvalueVar, Publisher):
